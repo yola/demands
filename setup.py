170919f06792291f1705c89e676044b833237f09
Michael Joseph <michaeljoseph+github@gmail.com>
2013-04-27 12:13:10 +0200

    Add author attribution

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name='demands',
     version='0.1.0',
     description='Base HTTP service client',
+    author='Yola',
+    author_email='tech@yola.com',
     url='https://github.com/yola/demands',
     packages=['demands'],
     install_requires=['requests < 1.0.0']
