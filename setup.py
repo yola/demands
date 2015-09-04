a23f6024164b420c76614a5628f5506dbc6f39c4
William Stewart <william@synthasite.com>
2015-09-04 12:10:30 +0200

    Actually use readme for long desc.

--- setup.py
+++ setup.py
@@ -5,10 +5,14 @@ from setuptools import setup
 init_py = open('demands/__init__.py').read()
 metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", init_py))
 
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
 setup(
     name='demands',
     version=metadata['version'],
     description=metadata['doc'],
+    long_description=readme,
     author='Yola',
     author_email='engineers@yola.com',
     license='MIT (Expat)',
