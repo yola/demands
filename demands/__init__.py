9a5f21931ea196dbbb41bf2658fcb05ccff752e7
Eugene Koval <eugene@yola.com>
2015-11-18 22:32:53 +0200

    Test

--- demands/__init__.py
+++ demands/__init__.py
@@ -105,6 +105,7 @@ class HTTPServiceClient(Session):
         if auth:
             log.debug('Authentication via HTTP auth as "%s"', auth[0])
 
+        response.is_ok = response.status_code < 300
         if self._is_ok(response):
             return response
         else:
@@ -114,7 +115,7 @@ class HTTPServiceClient(Session):
 
     def is_ok(self, response):
         expected_codes = request_params.get('expected_response_codes', [])
-        return (response.status_code < 300 or response.status_code in expected_codes)
+        return (response.is_ok or response.status_code in expected_codes)
 
     def _format_json_request(self, request_params):
         if request_params.get('send_as_json') and request_params.get('data'):
